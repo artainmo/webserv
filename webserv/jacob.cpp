4a2868fc23b63ad5b502f8d4a323de04f1271762
Yves Manzi <yv.manzi@gmail.com>
2021-01-08 18:42:33 +0100

    Update jacob.cpp
    merge in jacob.cpp

--- webserv/jacob.cpp
+++ webserv/jacob.cpp
@@ -12,10 +12,36 @@
 #include <netdb.h> 
 #include <arpa/inet.h>
 
+#include "main.hpp"
+
 #define PORT 80
 #define BUFSIZE 4096
 
 typedef struct sockaddr_in SA_IN ;
+
+std::string http_request_example = "GET / HTTP/1.1\r\n"
+			"Accept-Charsets: dwdwdq dwqdwqdwqdwq\r\n"
+            "Accept-Language: en-us\r\n"
+			"Allow: xwdwdw dwqdwqdwq\r\n"
+			"Authorization: dwdwqd dwqdwqdwq\r\n"
+			"Content-Language: dfeefe efewf\r\n"
+			"Content-Length: dwdwdw wdwdqw\r\n"
+			"Content-Location: sdwdwqdwdwd\r\n"
+			"Content-Type: ddw dwqdwqd\r\n"
+			"Date: 09-9w2-1w1\r\n"
+            "Host: localhost:8080\r\n"
+			"Last-Modified: 424443 3424324\r\n"
+			"Location: c;d';e\r\n"
+			"Referer: dwwdwqd dwqdqwdwq\r\n"
+			"Retry-After: dwdqwdw wdwqdqwdw\r\n"
+			"Server: dwdwdwdwdwdwd\r\n"
+			"Transfer-Encoding: fdsfsf34343\r\n"
+            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6\r\n"
+			"WWW_Authenticate: 2323wwdwdw\r\n"
+            "\r\n"
+            "Usually GET requests don\'t have a body\r\n"
+            "But I don\'t care in this case :)";
+
 
 void check(int val, std::string str)
 {
@@ -61,10 +87,11 @@ int	accept_new_connection(int & server_socket)
 
 void	*handle_connection(int & client_socket)
 {
-	char	buffer[BUFSIZE];
-	size_t	bytes_read;
+	char		buffer[BUFSIZE];
+	size_t		bytes_read;
 	int		msgsize = 0;
-	char	actualpath[PATH_MAX + 1];
+	char		actualpath[PATH_MAX + 1];
+	t_http_req	*req;
 
 	// clear the buffer
 	bzero(buffer, 4096);
@@ -73,6 +100,8 @@ void	*handle_connection(int & client_socket)
 	bytes_read = recv(client_socket, buffer, 4096, 0);
 	check(bytes_read, "recv error");
 
+// artainmo
+	req = parse_http_request(buffer);
 
 // Check si la connection doit rester ouverte ou pas
 
@@ -102,8 +131,18 @@ void	*handle_connection(int & client_socket)
 	return nullptr;
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
+	t_config *config;
+	t_http_req *req;
+
+	if (argc != 2)
+	{
+		std::cout << "ERROR: Wrong Program Arguments" << std::endl;
+		exit(1);
+	}
+	config = parse_config(argv[1]);
+
 	int server_socket = setup_server(PORT, SOMAXCONN);
 
 	fd_set current_sockets, ready_sockets;
