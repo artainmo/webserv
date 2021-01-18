250151f7344befd5549cde541f6689cb3fcbab59
Yves Manzi <ymanzi@shi-r11-p6.s19.be>
2021-01-18 18:11:30 +0100

    segafault

--- tests/lol/get-page.sh
+++ tests/lol/get-page.sh
@@ -2,9 +2,9 @@
 
 echo "open $1 $2"
 sleep 2
-echo "DELETE $3 HTTP/1.1"
+echo "POST $3 HTTP/1.1"
 echo "Host: $1"
-#echo "<html><body>Welcome to my HTTP SERVER !</body></html>"
+#echo "\r\n"
 echo
 echo
 sleep 2
